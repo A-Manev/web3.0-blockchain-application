6861626f64533abeb4595e91f036136cc1e10579
A-Manev <sasho_123_456@abv.bg>
2022-01-04 16:47:53 +0200

    Create components skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-const App = () => {
-  return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components"
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  )
-}
+    <div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  </div>
+);
+
 
 export default App
